0425612192040db2b2e18e8e098b497ea610b395
kaustubha s <90025152+kaustubha-kc@users.noreply.github.com>
2023-12-11 08:38:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const {app, BrowserWindow } = require('electron')
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title:'Image Resizer',
+        width:500,
+        height: 600
+    });
+
+    mainWindow.loadFile(path.join(__dirname,'./renderer/index.html'));
+}
+
+app.whenReady().then(()=>{
+    createMainWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
+      })
+});
